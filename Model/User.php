0e998099b572cc0f3859d4315886ec9077dd6b71
Cauan Cabral <cauan@radig.com.br>
2012-02-17 11:17:01 -0300

    Setting 'active' with 0 for non-verified users
    Updated test case

--- Model/User.php
+++ Model/User.php
@@ -656,7 +656,7 @@ class User extends UsersAppModel {
 		} else {
 			$postData[$this->alias]['email_verified'] = 1;
 		}
-		$postData[$this->alias]['active'] = 1;
+		$postData[$this->alias]['active'] = 0;
 		return $postData;
 	}
 
