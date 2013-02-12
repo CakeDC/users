2ac27ed2e17ba2ccfca744f2bc20e7b9b18a8ba0
Yevgeny Tomenko <skie@mail.ru>
2013-02-12 23:58:05 +0400

    Update license blocks.

--- Model/User.php
+++ Model/User.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Copyright 2010 - 2011, Cake Development Corporation (http://cakedc.com)
+ * Copyright 2010 - 2013, Cake Development Corporation (http://cakedc.com)
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright Copyright 2010 - 2011, Cake Development Corporation (http://cakedc.com)
+ * @copyright Copyright 2010 - 2013, Cake Development Corporation (http://cakedc.com)
  * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 
@@ -708,10 +708,9 @@ class User extends UsersAppModel {
 				$query['search'] = '';
 			}
 
-			$db =& ConnectionManager::getDataSource($this->useDbConfig);
+			$db = ConnectionManager::getDataSource($this->useDbConfig);
 			$by = $query['by'];
 			$search = $query['search'];
-			$byQuoted = $db->value($search);
 			$like = '%' . $query['search'] . '%';
 
 			switch ($by) {
