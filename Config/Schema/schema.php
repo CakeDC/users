c1c951bebcad523b550610f6d932acd917f510f9
Florian Krämer <florian.kraemer@cakedc.com>
2014-07-08 01:22:37 +0200

    CS fixes to schema.php

--- Config/Schema/schema.php
+++ Config/Schema/schema.php
@@ -15,46 +15,54 @@
  * @license   MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 class usersSchema extends CakeSchema {
-	var $name = 'users';
 
-	function before($event = array()) {
+	public $name = 'users';
+
+	public function before($event = array()) {
 		return true;
 	}
 
-	function after($event = array()) {
+	public function after($event = array()) {
 	}
 
-	var $user_details = array(
-		'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary'),
-		'user_id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36),
+	public $user_details = array(
+		'id' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 36, 'key' => 'primary'),
+		'user_id' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 36),
 		'position' => array('type' => 'float', 'null' => false, 'default' => '1'),
-		'field' => array('type' => 'string', 'null' => false, 'default' => NULL, 'key' => 'index'),
-		'value' => array('type' => 'text', 'null' => true, 'default' => NULL),
-		'input' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 16),
-		'data_type' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 16),
+		'field' => array('type' => 'string', 'null' => false, 'default' => null, 'key' => 'index'),
+		'value' => array('type' => 'text', 'null' => true, 'default' => null),
+		'input' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 16),
+		'data_type' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 16),
 		'label' => array('type' => 'string', 'null' => false, 'length' => 128),
-		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
-		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
-		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1), 'UNIQUE_PROFILE_PROPERTY' => array('column' => array('field', 'user_id'), 'unique' => 1))
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'UNIQUE_PROFILE_PROPERTY' => array('column' => array('field', 'user_id'), 'unique' => 1)
+		)
 	);
-	var $users = array(
-		'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary'),
-		'username' => array('type' => 'string', 'null' => false, 'default' => NULL, 'key' => 'index'),
-		'slug' => array('type' => 'string', 'null' => false, 'default' => NULL),
-		'password' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 128),
-		'password_token' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 128),
-		'email' => array('type' => 'string', 'null' => true, 'default' => NULL, 'key' => 'index'),
+
+	public $users = array(
+		'id' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 36, 'key' => 'primary'),
+		'username' => array('type' => 'string', 'null' => false, 'default' => null, 'key' => 'index'),
+		'slug' => array('type' => 'string', 'null' => false, 'default' => null),
+		'password' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 128),
+		'password_token' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 128),
+		'email' => array('type' => 'string', 'null' => true, 'default' => null, 'key' => 'index'),
 		'email_verified' => array('type' => 'boolean', 'null' => true, 'default' => '0'),
-		'email_token' => array('type' => 'string', 'null' => true, 'default' => NULL),
-		'email_token_expires' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'email_token' => array('type' => 'string', 'null' => true, 'default' => null),
+		'email_token_expires' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'tos' => array('type' => 'boolean', 'null' => true, 'default' => '0'),
 		'active' => array('type' => 'boolean', 'null' => true, 'default' => '0'),
-		'last_login' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
-		'last_action' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'last_login' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'last_action' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'is_admin' => array('type' => 'boolean', 'null' => true, 'default' => '0'),
-		'role' => array('type' => 'string', 'null' => true, 'default' => NULL),
-		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
-		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
-		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1), 'BY_USERNAME' => array('column' => array('username'), 'unique' => 0), 'BY_EMAIL' => array('column' => array('email'), 'unique' => 0))
+		'role' => array('type' => 'string', 'null' => true, 'default' => null),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'BY_USERNAME' => array('column' => array('username'), 'unique' => 0),
+			'BY_EMAIL' => array('column' => array('email'), 'unique' => 0)
+		)
 	);
 }
