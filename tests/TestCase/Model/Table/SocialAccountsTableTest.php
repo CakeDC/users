357a3bc1afbcc7b968c89c179643a2ed1b1e1970
jorge gonzalez <steinkel@gmail.com>
2015-09-14 09:02:24 +0000

    add test to SocialAccountsTable

--- tests/TestCase/Model/Table/SocialAccountsTableTest.php
+++ tests/TestCase/Model/Table/SocialAccountsTableTest.php
@@ -44,12 +44,6 @@ class SocialAccountsTableTest extends TestCase
     {
         parent::setUp();
         $this->SocialAccounts = TableRegistry::get('CakeDC/Users.SocialAccounts');
-        $this->fullBaseBackup = Router::fullBaseUrl();
-        Router::fullBaseUrl('http://users.test');
-        Email::configTransport('test', [
-            'className' => 'Debug'
-        ]);
-        $this->Email = new Email(['from' => 'test@example.com', 'transport' => 'test']);
     }
 
     /**
@@ -60,71 +54,21 @@ class SocialAccountsTableTest extends TestCase
     public function tearDown()
     {
         unset($this->SocialAccounts);
-        Router::fullBaseUrl($this->fullBaseBackup);
-        Email::dropTransport('test');
 
         parent::tearDown();
     }
 
-    /**
-     * Test validateEmail method
-     *
-     * @return void
-     */
-    public function testValidateEmail()
+    public function testValidationHappy()
     {
-        $token = 'token-1234';
-        $result = $this->SocialAccounts->validateAccount(SocialAccountsTable::PROVIDER_FACEBOOK, 'reference-1-1234', $token);
-        $this->assertTrue($result->active);
-        $this->assertEquals($token, $result->token);
-    }
-
-    /**
-     * Test validateEmail method
-     *
-     * @expectedException \Cake\Datasource\Exception\RecordNotFoundException
-     */
-    public function testValidateEmailInvalidToken()
-    {
-        $this->SocialAccounts->validateAccount(1, 'reference-1234', 'invalid-token');
-    }
-
-    /**
-     * Test validateEmail method
-     *
-     * @expectedException \Cake\Datasource\Exception\RecordNotFoundException
-     */
-    public function testValidateEmailInvalidUser()
-    {
-        $this->SocialAccounts->validateAccount(1, 'invalid-user', 'token-1234');
-    }
-
-    /**
-     * Test validateEmail method
-     *
-     * @expectedException CakeDC\Users\Exception\AccountAlreadyActiveException
-     */
-    public function testValidateEmailActiveAccount()
-    {
-        $this->SocialAccounts->validateAccount(SocialAccountsTable::PROVIDER_TWITTER, 'reference-1-1234', 'token-1234');
-    }
-
-    /**
-     * Test sendSocialValidationEmail method
-     *
-     * @return void
-     */
-    public function testSendSocialValidationEmail()
-    {
-        $this->markTestIncomplete('move to SocialAccountBehaviorTest');
-        $user = $this->SocialAccounts->find()->contain('Users')->first();
-        $this->Email->emailFormat('both');
-        $result = $this->SocialAccounts->sendSocialValidationEmail($user, $user->user, $this->Email);
-        $this->assertTextContains('From: test@example.com', $result['headers']);
-        $this->assertTextContains('To: user-1@test.com', $result['headers']);
-        $this->assertTextContains('Subject: first1, Your social account validation link', $result['headers']);
-        $this->assertTextContains('Hi first1,', $result['message']);
-        $this->assertTextContains('<a href="http://users.test/users/social-accounts/validate-account/Facebook/reference-1-1234/token-1234">Activate your social login here</a>', $result['message']);
-        $this->assertTextContains('If the link is not correcly displayed, please copy the following address in your web browser http://users.test/users/social-accounts/validate-account/Facebook/reference-1-1234/token-1234', $result['message']);
+        $data = [
+            'provider' => 'Facebook',
+            'reference' => 'test-reference',
+            'link' => 'test-link',
+            'token' => 'test-token',
+            'active' => 0,
+            'data' => 'test-data',
+        ];
+        $entity = $this->SocialAccounts->newEntity($data);
+        $this->assertEmpty($entity->errors());
     }
 }
