8101e231b3a074ce69565b645f52d3128ae19437
Jose Alejandro Alvarez Mendoza <jose.mendoza@cakedc.com>
2015-12-02 21:47:47 -0430

    Changing SessionComponent::setFlash for FlashComponent::set

--- Test/Case/Controller/UsersControllerTest.php
+++ Test/Case/Controller/UsersControllerTest.php
@@ -13,6 +13,7 @@ App::uses('UsersController', 'Users.Controller');
 App::uses('User', 'Users.Model');
 App::uses('AuthComponent', 'Controller/Component');
 App::uses('CookieComponent', 'Controller/Component');
+App::uses('FlashComponent', 'Controller/Component');
 App::uses('SessionComponent', 'Controller/Component');
 App::uses('RememberMeComponent', 'Users.Controller/Component');
 App::uses('Security', 'Utility');
@@ -257,13 +258,15 @@ class UsersControllerTestCase extends CakeTestCase {
 		$this->Users->request->url = '/users/users/login';
 
 		$this->Collection = $this->getMock('ComponentCollection');
-		$session = $this->getMock('SessionComponent', array('setFlash'), array($this->Collection));
-		$this->Users->Session = $session;
-		$this->Users->Session->expects($this->any())
-			->method('setFlash')
+//		$session = $this->getMock('SessionComponent', array('setFlash'), array($this->Collection));
+//		$this->Users->Session = $session;
+        $flash = $this->getMock('FlashComponent', array('set'), array($this->Collection));
+        $this->Users->Flash = $flash;
+		$this->Users->Flash->expects($this->any())
+			->method('set')
 			->with(__d('users', 'adminuser you have successfully logged in'));
 		$this->Users->Auth = $this->getMock('AuthComponent', array('login', 'user', 'redirectUrl'), array($this->Collection));
-		$this->Users->Auth->Session = $session;
+		$this->Users->Auth->Flash= $flash;
 		$this->Users->Auth->expects($this->once())
 			->method('login')
 			->will($this->returnValue(true));
@@ -291,6 +294,7 @@ class UsersControllerTestCase extends CakeTestCase {
 
 		$this->Users->login();
 		$this->assertEquals(Router::normalize($this->Users->redirectUrl), Router::normalize(Router::url($this->Users->Auth->loginRedirect)));
+        die;
 	}
 
 /**
@@ -305,9 +309,9 @@ class UsersControllerTestCase extends CakeTestCase {
 		$this->__setGet();
 		$this->Users->login();
 		$this->Collection = $this->getMock('ComponentCollection');
-		$this->Users->Session = $this->getMock('SessionComponent', array('setFlash'), array($this->Collection));
-		$this->Users->Session->expects($this->never())
-			->method('setFlash');
+		$this->Users->Flash = $this->getMock('FlashComponent', array('set'), array($this->Collection));
+		$this->Users->Flash->expects($this->never())
+			->method('set');
 	}
 
 /**
@@ -348,9 +352,9 @@ class UsersControllerTestCase extends CakeTestCase {
 				'tos' => 1)));
 		$this->Users->beforeFilter();
 		$this->Collection = $this->getMock('ComponentCollection');
-		$this->Users->Session = $this->getMock('SessionComponent', array('setFlash'), array($this->Collection));
-		$this->Users->Session->expects($this->once())
-			->method('setFlash')
+		$this->Users->Flash = $this->getMock('FlashComponent', array('set'), array($this->Collection));
+		$this->Users->Flash->expects($this->once())
+			->method('set')
 			->with(__d('users', 'Your account has been created. You should receive an e-mail shortly to authenticate your account. Once validated you will be able to login.'));
 		$this->Users->add();
 		$this->__setPost(array(
@@ -361,9 +365,9 @@ class UsersControllerTestCase extends CakeTestCase {
 				'temppassword' => '',
 				'tos' => 0)));
 		$this->Users->beforeFilter();
-		$this->Users->Session = $this->getMock('SessionComponent', array('setFlash'), array($this->Collection));
-		$this->Users->Session->expects($this->once())
-			->method('setFlash')
+		$this->Users->Flash = $this->getMock('FlashComponent', array('set'), array($this->Collection));
+		$this->Users->Flash->expects($this->once())
+			->method('set')
 			->with(__d('users', 'Your account could not be created. Please, try again.'));
 		$this->Users->add();
 	}
@@ -378,17 +382,17 @@ class UsersControllerTestCase extends CakeTestCase {
 		$this->Users->User->id = '37ea303a-3bdc-4251-b315-1316c0b300fa';
 		$this->Users->User->saveField('email_token_expires', date('Y-m-d H:i:s', strtotime('+1 year')));
 		$this->Collection = $this->getMock('ComponentCollection');
-		$this->Users->Session = $this->getMock('SessionComponent', array('setFlash'), array($this->Collection));
-		$this->Users->Session->expects($this->once())
-			->method('setFlash')
+		$this->Users->Flash = $this->getMock('FlashComponent', array('set'), array($this->Collection));
+		$this->Users->Flash->expects($this->once())
+			->method('set')
 			->with(__d('users', 'Your e-mail has been validated!'));
 
 		$this->Users->verify('email', 'testtoken2');
 
 		$this->Users->beforeFilter();
-		$this->Users->Session = $this->getMock('SessionComponent', array('setFlash'), array($this->Collection));
-		$this->Users->Session->expects($this->once())
-			->method('setFlash')
+		$this->Users->Flash = $this->getMock('FlashComponent', array('set'), array($this->Collection));
+		$this->Users->Flash->expects($this->once())
+			->method('set')
 			->with(__d('users', 'Invalid token, please check the email you were sent, and retry the verification link.'));
 
 		$this->Users->verify('email', 'invalid-token');
@@ -403,9 +407,10 @@ class UsersControllerTestCase extends CakeTestCase {
 		$this->Users->beforeFilter();
 		$this->Collection = $this->getMock('ComponentCollection');
 		$this->Users->Cookie = $this->getMock('CookieComponent', array(), array($this->Collection));
-		$this->Users->Session = $this->getMock('SessionComponent', array('setFlash', 'destroy'), array($this->Collection));
-		$this->Users->Session->expects($this->once())
-			->method('setFlash')
+		$this->Users->Session = $this->getMock('SessionComponent', array('destroy'), array($this->Collection));
+		$this->Users->Flash = $this->getMock('FlashComponent', array('set'), array($this->Collection));
+		$this->Users->Flash->expects($this->once())
+			->method('set')
 			->with(__d('users', 'testuser you have successfully logged out'));
 		$this->Users->Session->expects($this->once())
 			->method('destroy');
