f28c56b50acf9200565eb0830f1ed88dd5fd672b
Jorge González <steinkel@gmail.com>
2016-03-07 10:02:59 +0000

    refs #279 refactor User entity to use Time, add unit tests to check es_AR locale

--- tests/TestCase/Model/Entity/UserTest.php
+++ tests/TestCase/Model/Entity/UserTest.php
@@ -3,6 +3,8 @@ namespace CakeDC\Users\Test\TestCase\Model\Entity;
 
 use CakeDC\Users\Model\Entity\User;
 use Cake\Auth\DefaultPasswordHasher;
+use Cake\I18n\I18n;
+use Cake\I18n\Time;
 use Cake\TestSuite\TestCase;
 
 /**
@@ -10,7 +12,6 @@ use Cake\TestSuite\TestCase;
  */
 class UserTest extends TestCase
 {
-
     /**
      * setUp method
      *
@@ -70,6 +71,27 @@ class UserTest extends TestCase
         $this->assertFalse($isExpired);
     }
 
+    /**
+     * Test tokenExpired another locale
+     *
+     * @return void
+     */
+    public function testTokenExpiredLocale()
+    {
+        I18n::locale('es_AR');
+        $this->User->token_expires = '+1 day';
+        $isExpired = $this->User->tokenExpired();
+        $this->assertFalse($isExpired);
+        $this->User->token_expires = '-1 day';
+        $isExpired = $this->User->tokenExpired();
+        $this->assertTrue($isExpired);
+    }
+
+    /**
+     * test
+     *
+     * @return void
+     */
     public function testPasswordsAreEncrypted()
     {
         $pw = 'password';
@@ -84,6 +106,11 @@ class UserTest extends TestCase
         $this->assertTrue((new DefaultPasswordHasher)->check($pw, $this->User->confirm_password));
     }
 
+    /**
+     * test
+     *
+     * @return void
+     */
     public function testCheckPassword()
     {
         $pw = 'password';
@@ -91,6 +118,11 @@ class UserTest extends TestCase
         $this->assertFalse($this->User->checkPassword($pw, 'fail'));
     }
 
+    /**
+     * test
+     *
+     * @return void
+     */
     public function testGetAvatar()
     {
         $this->assertNull($this->User->avatar);
@@ -100,5 +132,69 @@ class UserTest extends TestCase
             ['avatar' => 'second-avatar']
         ];
         $this->assertSame($avatar, $this->User->avatar);
+    }
+
+    /**
+     * test
+     *
+     * @return void
+     */
+    public function testUpdateToken()
+    {
+        $now = Time::now();
+        Time::setTestNow($now);
+        $this->assertNull($this->User['token']);
+        $this->assertNull($this->User['token_expires']);
+        $this->User->updateToken();
+        $this->assertEquals($now, $this->User['token_expires']);
+        $this->assertNotNull($this->User['token']);
+    }
+
+    /**
+     * test
+     *
+     * @return void
+     */
+    public function testUpdateTokenExisting()
+    {
+        $now = Time::now();
+        Time::setTestNow($now);
+        $this->User['token'] = 'aaa';
+        $this->User['token_expires'] = $now;
+        $this->User->updateToken();
+        $this->assertEquals($now, $this->User['token_expires']);
+        $this->assertNotEquals('aaa', $this->User['token']);
+    }
+
+    /**
+     * test
+     *
+     * @return void
+     */
+    public function testUpdateTokenAdd()
+    {
+        $now = Time::now();
+        Time::setTestNow($now);
+        $this->assertNull($this->User['token']);
+        $this->assertNull($this->User['token_expires']);
+        $this->User->updateToken(20);
+        $this->assertEquals($now->addSeconds(20), $this->User['token_expires']);
+        $this->assertNotNull($this->User['token']);
+    }
+
+    /**
+     * test
+     *
+     * @return void
+     */
+    public function testUpdateTokenExistingAdd()
+    {
+        $now = Time::now();
+        Time::setTestNow($now);
+        $this->User['token'] = 'aaa';
+        $this->User['token_expires'] = $now;
+        $this->User->updateToken(20);
+        $this->assertEquals($now->addSecond(20), $this->User['token_expires']);
+        $this->assertNotEquals('aaa', $this->User['token']);
     }
 }
