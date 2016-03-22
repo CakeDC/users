89e0ea930623233c9df5b2d7a43d8d802a51ad82
Pedro Fernandes Steimbruch <pedro.steimbruch@cakedc.com>
2016-03-22 14:49:20 -0300

    Refs #306 adding email config to enable it on tests

--- tests/TestCase/Controller/Traits/BaseTraitTest.php
+++ tests/TestCase/Controller/Traits/BaseTraitTest.php
@@ -185,4 +185,21 @@ abstract class BaseTraitTest extends TestCase
                 ->method('dispatchEvent')
                 ->will($this->returnValue($event));
     }
+
+    /**
+     * Config email
+     *
+     * @return void
+     */
+    protected function _configEmail()
+    {
+        \Cake\Mailer\Email::configTransport('default', [
+            'className' => 'Debug'
+        ]);
+        \Cake\Mailer\Email::config('default', [
+            'transport' => 'default',
+            'from' => 'test@test.com',
+            'log' => true
+        ]);
+    }
 }
