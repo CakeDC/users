78db7ad39f47dcb14862d1a8ac511a92913b1f29
Marcelo Rocha <contato@omarcelo.com.br>
2018-05-05 12:05:03 -0300

    Added custom authenticators

---
+++ src/Authenticator/CookieAuthenticator.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace CakeDC\Users\Authenticator;
+
+use Authentication\Authenticator\PersistenceInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+use Authentication\Authenticator\CookieAuthenticator as BaseAuthenticator;
+
+/**
+ * Cookie Authenticator
+ *
+ * Authenticates an identity based on a cookies data.
+ */
+class CookieAuthenticator extends BaseAuthenticator implements PersistenceInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function persistIdentity(ServerRequestInterface $request, ResponseInterface $response, $identity)
+    {
+        $field = $this->getConfig('rememberMeField');
+
+        $bodyData = $request->getParsedBody();
+        if (empty($bodyData)) {
+            $session = $request->getAttribute('session');
+            $bodyData = $session->read('CookieAuth');
+            $session->delete('CookieAuth');
+        }
+
+        if (!$this->_checkUrl($request) || !is_array($bodyData) || empty($bodyData[$field])) {
+            return [
+                'request' => $request,
+                'response' => $response
+            ];
+        }
+
+        $value = $this->_createToken($identity);
+        $cookie = $this->_createCookie($value);
+
+        return [
+            'request' => $request,
+            'response' => $response->withAddedHeader('Set-Cookie', $cookie->toHeaderValue())
+        ];
+    }
+}
