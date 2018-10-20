459da3c1149e41e316608bffe0b87ee94b84b772
Marcelo Rocha <contato@omarcelo.com.br>
2018-10-20 16:29:12 -0300

    Social data mappers don't have $rawData property they expect an argument for invoke method

--- tests/TestCase/Controller/Traits/BaseTraitTest.php
+++ tests/TestCase/Controller/Traits/BaseTraitTest.php
@@ -11,10 +11,10 @@
 
 namespace CakeDC\Users\Test\TestCase\Controller\Traits;
 
-use Authentication\AuthenticationService;
 use Authentication\Authenticator\Result;
 use Authentication\Controller\Component\AuthenticationComponent;
 use Authentication\Identity;
+use CakeDC\Users\Authentication\AuthenticationService;
 use CakeDC\Users\Model\Entity\User;
 use Cake\Controller\ComponentRegistry;
 use Cake\Controller\Controller;
