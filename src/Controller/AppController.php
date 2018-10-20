ebb569e7851515d398666708c3ca8934d3ff6f25
Marcelo Rocha <contato@omarcelo.com.br>
2018-10-20 15:17:47 -0300

    We should load auth components at users controller not app controller. Permission is already set by permisssions.php

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -12,7 +12,6 @@
 namespace CakeDC\Users\Controller;
 
 use App\Controller\AppController as BaseController;
-use Cake\Core\Configure;
 
 /**
  * AppController for Users Plugin
@@ -20,27 +19,6 @@ use Cake\Core\Configure;
  */
 class AppController extends BaseController
 {
-    protected $_defaultAuthorizationConfig = [
-        'skipAuthorization' => [
-            'validateAccount',
-            // LoginTrait
-            'socialLogin',
-            'login',
-            'logout',
-            'socialEmail',
-            'verify',
-            // RegisterTrait
-            'register',
-            'validateEmail',
-            // PasswordManagementTrait used in RegisterTrait
-            'changePassword',
-            'resetPassword',
-            'requestResetPassword',
-            // UserValidationTrait used in PasswordManagementTrait
-            'resendTokenValidation',
-        ]
-    ];
-
     /**
      * Initialize
      *
@@ -52,17 +30,6 @@ class AppController extends BaseController
         $this->loadComponent('Security');
         if ($this->request->getParam('_csrfToken') === false) {
             $this->loadComponent('Csrf');
-        }
-        $this->loadComponent('Authentication.Authentication', Configure::read('Auth.AuthenticationComponent'));
-
-        if (Configure::read('Auth.AuthorizationComponent.enable') !== false) {
-            $config = (array)Configure::read('Auth.AuthorizationComponent') + $this->_defaultAuthorizationConfig;
-
-            $this->loadComponent('Authorization.Authorization', $config);
-        }
-
-        if (Configure::read('Users.GoogleAuthenticator.login') !== false) {
-            $this->loadComponent('CakeDC/Users.GoogleAuthenticator');
         }
     }
 }
