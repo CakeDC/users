0943d13429abead060c126e6e3101459eb071de0
Marcelo Rocha <contato@omarcelo.com.br>
2018-11-03 15:27:28 -0300

    minor refactor on social auth

--- src/Middleware/SocialEmailMiddleware.php
+++ src/Middleware/SocialEmailMiddleware.php
@@ -5,7 +5,6 @@ namespace CakeDC\Users\Middleware;
 use Cake\Core\Configure;
 use Cake\Http\Exception\NotFoundException;
 use Cake\Http\ServerRequest;
-use CakeDC\Users\Exception\SocialAuthenticationException;
 use Psr\Http\Message\ResponseInterface;
 
 class SocialEmailMiddleware extends SocialAuthMiddleware
@@ -20,34 +19,17 @@ class SocialEmailMiddleware extends SocialAuthMiddleware
      */
     public function __invoke(ServerRequest $request, ResponseInterface $response, $next)
     {
-        if ($request->getAttribute('identity')) {
-            $request->getSession()->delete(Configure::read('Users.Key.Session.social'));
-        }
         $action = $request->getParam('action');
         if ($action !== 'socialEmail' || $request->getParam('plugin') !== 'CakeDC/Users') {
+            $request->getSession()->delete(Configure::read('Users.Key.Session.social'));
+
             return $next($request, $response);
         }
 
-        return $this->handleAction($request, $response, $next);
-    }
-
-    /**
-     * Handle social email step post.
-     *
-     * @param int $request authentication result
-     * @param \Psr\Http\Message\ServerRequestInterface $response The request.
-     * @param callable $next The response.
-     * @return \Psr\Http\Message\ResponseInterface A response
-     */
-    private function handleAction(ServerRequest $request, ResponseInterface $response, $next)
-    {
         if (!$request->getSession()->check(Configure::read('Users.Key.Session.social'))) {
             throw new NotFoundException();
         }
-        try {
-            return $next($request, $response);
-        } catch (SocialAuthenticationException $exception) {
-            return $this->onAuthenticationException($request, $response, $exception);
-        }
+
+        return $this->goNext($request, $response, $next);
     }
 }
