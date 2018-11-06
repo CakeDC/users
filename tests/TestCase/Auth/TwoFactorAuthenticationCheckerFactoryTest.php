528b9b55982414c94beca1b4d942700085fb04d5
Marcelo Rocha <contato@omarcelo.com.br>
2018-11-06 09:55:37 -0200

    update file doc

--- tests/TestCase/Auth/TwoFactorAuthenticationCheckerFactoryTest.php
+++ tests/TestCase/Auth/TwoFactorAuthenticationCheckerFactoryTest.php
@@ -1,10 +1,19 @@
 <?php
+/**
+ * Copyright 2010 - 2018, Cake Development Corporation (https://www.cakedc.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright 2010 - 2018, Cake Development Corporation (https://www.cakedc.com)
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
 namespace CakeDC\Users\Test\TestCase\Auth;
 
-use Cake\Core\Configure;
 use CakeDC\Users\Auth\DefaultTwoFactorAuthenticationChecker;
 use CakeDC\Users\Auth\TwoFactorAuthenticationCheckerFactory;
 use CakeDC\Users\Auth\TwoFactorAuthenticationCheckerInterface;
+use Cake\Core\Configure;
 use Cake\TestSuite\TestCase;
 
 class TwoFactorAuthenticationCheckerFactoryTest extends TestCase
