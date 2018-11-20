4ac899383f593d9a7ae6efc40f86ef87ffcd864e
Marcelo Rocha <contato@omarcelo.com.br>
2018-11-20 14:21:47 -0200

    Moved IsAuthorizedTrait to auth plugin

--- src/View/Helper/AuthLinkHelper.php
+++ src/View/Helper/AuthLinkHelper.php
@@ -1,17 +1,17 @@
 <?php
 /**
- * Copyright 2010 - 2017, Cake Development Corporation (https://www.cakedc.com)
+ * Copyright 2010 - 2018, Cake Development Corporation (https://www.cakedc.com)
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright Copyright 2010 - 2017, Cake Development Corporation (https://www.cakedc.com)
+ * @copyright Copyright 2010 - 2018, Cake Development Corporation (https://www.cakedc.com)
  * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 
 namespace CakeDC\Users\View\Helper;
 
-use CakeDC\Users\Traits\IsAuthorizedTrait;
+use CakeDC\Auth\Traits\IsAuthorizedTrait;
 use Cake\Utility\Hash;
 use Cake\View\Helper\HtmlHelper;
 
