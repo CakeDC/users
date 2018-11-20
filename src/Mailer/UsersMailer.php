61280599ab5480ad739f93d8709320fe17c6c98c
Marcelo Rocha <contato@omarcelo.com.br>
2018-11-20 16:27:22 -0200

    making sure files has copyright

--- src/Mailer/UsersMailer.php
+++ src/Mailer/UsersMailer.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Copyright 2010 - 2017, Cake Development Corporation (https://www.cakedc.com)
+ * Copyright 2010 - 2018, Cake Development Corporation (https://www.cakedc.com)
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright Copyright 2010 - 2017, Cake Development Corporation (https://www.cakedc.com)
+ * @copyright Copyright 2010 - 2018, Cake Development Corporation (https://www.cakedc.com)
  * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 namespace CakeDC\Users\Mailer;
