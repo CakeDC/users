496979909c3263449dcddb43cbd0c882b5577e2f
Marcelo Rocha <contato@omarcelo.com.br>
2019-02-08 11:40:32 -0200

    Fixing fixture table options

--- tests/Fixture/UsersFixture.php
+++ tests/Fixture/UsersFixture.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright 2010 - 2017, Cake Development Corporation (https://www.cakedc.com)
+ * Copyright 2010 - 2019, Cake Development Corporation (https://www.cakedc.com)
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
