b94f29e7d71cd8881d923af7acab785236fe6401
Marcelo Rocha <contato@omarcelo.com.br>
2019-07-25 12:43:34 -0300

    phpunit fix

--- tests/TestCase/Controller/Traits/PasswordManagementTraitTest.php
+++ tests/TestCase/Controller/Traits/PasswordManagementTraitTest.php
@@ -61,7 +61,7 @@ class PasswordManagementTraitTest extends BaseTraitTest
                 ]));
         $this->Trait->expects($this->once())
                 ->method('redirect')
-                ->with(['plugin' => 'CakeDC/Users', 'controller' => 'Users', 'action' => 'profile']);
+                ->with(['prefix' => false, 'plugin' => 'CakeDC/Users', 'controller' => 'Users', 'action' => 'profile']);
         $this->Trait->Flash->expects($this->any())
             ->method('success')
             ->with('Password has been changed successfully');
