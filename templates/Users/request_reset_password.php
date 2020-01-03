64bf25414ca53f3559a0fbb2366a3459b79fb382
Dan <email@danielupshaw.com>
2020-01-03 12:38:31 -0500

    Using entity for `$this->Form->create()`

--- templates/Users/request_reset_password.php
+++ templates/Users/request_reset_password.php
@@ -1,6 +1,9 @@
+<?php
+use CakeDC\Users\Model\Entity\User;
+?>
 <div class="users form">
     <?= $this->Flash->render('auth') ?>
-    <?= $this->Form->create('User') ?>
+    <?= $this->Form->create(new User) ?>
     <fieldset>
         <legend><?= __d('cake_d_c/users', 'Please enter your email to reset your password') ?></legend>
         <?= $this->Form->control('reference') ?>
