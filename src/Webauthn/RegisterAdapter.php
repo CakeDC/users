4ff0180794807ada7e2b87469d3484be5a7ba38e
Marcelo Rocha <contato@omarcelo.com.br>
2021-12-20 12:19:21 -0300

    coding standard

--- src/Webauthn/RegisterAdapter.php
+++ src/Webauthn/RegisterAdapter.php
@@ -1,9 +1,19 @@
 <?php
+declare(strict_types=1);
+
+/**
+ * Copyright 2010 - 2021, Cake Development Corporation (https://www.cakedc.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright 2010 - 2019, Cake Development Corporation (https://www.cakedc.com)
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
 
 namespace CakeDC\Users\Webauthn;
 
 use Webauthn\PublicKeyCredentialCreationOptions;
-
 
 class RegisterAdapter extends BaseAdapter
 {
@@ -32,6 +42,9 @@ class RegisterAdapter extends BaseAdapter
      */
     public function verifyResponse(): \Webauthn\PublicKeyCredentialSource
     {
+        /**
+         *
+         */
         $options = $this->request->getSession()->read('Webauthn2fa.registerOptions');
         $credential = $this->loadAndCheckAttestationResponse($options);
         $this->repository->saveCredentialSource($credential);
@@ -40,7 +53,7 @@ class RegisterAdapter extends BaseAdapter
     }
 
     /**
-     * @param $options
+     * @param \Webauthn\PublicKeyCredentialCreationOptions $options
      * @return \Webauthn\PublicKeyCredentialSource
      */
     protected function loadAndCheckAttestationResponse($options): \Webauthn\PublicKeyCredentialSource
