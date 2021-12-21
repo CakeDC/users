1ba39937a74969a38afbf6d6c20c3c369b4294c7
Marcelo Rocha <contato@omarcelo.com.br>
2021-12-21 07:17:37 -0300

    improved translation message lookup

--- templates/Users/webauthn2fa.php
+++ templates/Users/webauthn2fa.php
@@ -15,16 +15,16 @@ $this->assign('title', __('Two-factor authentication'));
                 <?= $this->Flash->render() ?>
                 <fieldset>
                     <div id="webauthn2faRegisterInfo" style="display:none;">
-                        <h2 class='text-center'><?= __('Registering your yubico key')?> </h2>
-                        <h3 class='text-center'><?= __('Please insert and tap your yubico key')?></h3>
-                        <p>In order to enable your YubiKey the first step is to perform a registration.</p>
-                        <p>When the YubiKey starts blinking, press the golden disc to activate it. Depending on the web browser you might need to confirm the use of extended information from the YubiKey.</p>
+                        <h2 class='text-center'><?= __d('cake_d_c/users', 'Registering your yubico key')?> </h2>
+                        <h3 class='text-center'><?= __d('cake_d_c/users', 'Please insert and tap your yubico key')?></h3>
+                        <p><?= __d('cake_d_c/users', 'In order to enable your YubiKey the first step is to perform a registration.')?></p>
+                        <p><?= __d('cake_d_c/users', 'When the YubiKey starts blinking, press the golden disc to activate it. Depending on the web browser you might need to confirm the use of extended information from the YubiKey.')?></p>
                     </div>
                     <div id="webauthn2faAuthenticateInfo" style="display:none;">
-                        <h4 class='text-center'><?= __('Verify your registered yubico key')?> </h4>
-                        <h3 class='text-center'><?= __('Please insert and tap your yubico key')?></h3>
-                        <p><?__( 'You can now finish the authentication process using the registered device.')?></p>
-                        <p><?= __('When the YubiKey starts blinking, press the golden disc to activate it. Depending on the web browser you might need to confirm the use of extended information from the YubiKey.')?></p>
+                        <h4 class='text-center'><?= __d('cake_d_c/users', 'Verify your registered yubico key')?> </h4>
+                        <h3 class='text-center'><?= __d('cake_d_c/users', 'Please insert and tap your yubico key')?></h3>
+                        <p><?= __d('cake_d_c/users', 'You can now finish the authentication process using the registered device.')?></p>
+                        <p><?= __d('cake_d_c/users', 'When the YubiKey starts blinking, press the golden disc to activate it. Depending on the web browser you might need to confirm the use of extended information from the YubiKey.')?></p>
                     </div>
                     <p class="text-center"><?= $this->Html->link(
                             __('Reload'),
