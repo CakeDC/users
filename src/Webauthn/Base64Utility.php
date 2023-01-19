033eabb3341b5a9e76fc5ba03e92c07f04bbeeec
Marcelo Rocha <contato@omarcelo.com.br>
2023-01-19 14:06:03 -0300

    Upgrade to CakePHP 5 - Added utilility for base64 compatibility with Webauthn feature

---
+++ src/Webauthn/Base64Utility.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Copyright 2010 - 2023, Cake Development Corporation (https://www.cakedc.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright 2010 - 2018, Cake Development Corporation (https://www.cakedc.com)
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+namespace CakeDC\Users\Webauthn;
+
+use InvalidArgumentException;
+use ParagonIE\ConstantTime\Base64UrlSafe;
+
+class Base64Utility
+{
+    /**
+     * @param string $data
+     * @return string
+     */
+    public static function complyEncodedNoPadding(string $data): string
+    {
+        return Base64UrlSafe::encodeUnpadded(static::basicDecode($data));
+    }
+
+    /**
+     * @param string $data The data to encode
+     * @param bool   $usePadding If true, the "=" padding at end of the encoded value are kept, else it is removed
+     *
+     * @return string The data encoded
+     */
+    public static function basicEncode(string $data, bool $usePadding = false): string
+    {
+        $encoded = strtr(base64_encode($data), '+/', '-_');
+
+        return true === $usePadding ? $encoded : rtrim($encoded, '=');
+    }
+
+    /**
+     * @param string $data The data to decode
+     *
+     * @throws InvalidArgumentException
+     *
+     * @return string The data decoded
+     */
+    public static function basicDecode(string $data): string
+    {
+        $decoded = base64_decode(strtr($data, '-_', '+/'), true);
+        if (false === $decoded) {
+            throw new InvalidArgumentException('Invalid data provided');
+        }
+
+        return $decoded;
+    }
+}
---
+++ tests/TestCase/Webauthn/Base64UtilityTest.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * Copyright 2010 - 2023, Cake Development Corporation (https://www.cakedc.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright 2010 - 2018, Cake Development Corporation (https://www.cakedc.com)
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+namespace CakeDC\Users\Test\TestCase\Webauthn;
+
+use Cake\TestSuite\TestCase;
+use CakeDC\Users\Webauthn\Base64Utility;
+use ParagonIE\ConstantTime\Base64UrlSafe;
+
+class Base64UtilityTest extends TestCase
+{
+    /**
+     * Test methods basicEncode and basicDecode
+     *
+     * @param string $originalText
+     * @dataProvider dataProviderBasicEncodeDecodeText
+     * @return void
+     */
+    public function testBasicEncodeDecode($originalText)
+    {
+        $encoded = Base64Utility::basicEncode($originalText);
+        $this->assertNotEmpty($encoded);
+        $this->assertStringEndsNotWith('=', $encoded);
+        $decoded = Base64Utility::basicDecode($encoded);
+        $this->assertSame($originalText, $decoded);
+    }
+
+    /**
+     * @return \string[][]
+     */
+    public function dataProviderBasicEncodeDecodeText(): array
+    {
+        return [
+            ['00000000-0000-0000-0000-000000000002'],
+            ['$2y$10$Nvu7ipP.z8tiIl75OdUvt.86vuG6iKMoHIOc7O7mboFI85hSyTEde'],
+            ['First Name']
+        ];
+    }
+
+    /**
+     * Test method complyEncodedNoPadding
+     *
+     * @param string $encodedTextWithPadding
+     * @param string $originalText
+     * @dataProvider dataProviderComplyEncodedNoPadding
+     * @return void
+     */
+    public function testComplyEncodedNoPadding($encodedTextWithPadding, $originalText)
+    {
+        $encoded = Base64Utility::complyEncodedNoPadding($encodedTextWithPadding);
+        $this->assertNotEmpty($encoded);
+        $this->assertStringEndsNotWith('=', $encoded);
+        $decoded = Base64UrlSafe::decodeNoPadding($encoded);
+        $this->assertSame($originalText, $decoded);
+    }
+
+    /**
+     * @return \string[][]
+     */
+    public function dataProviderComplyEncodedNoPadding(): array
+    {
+        return [
+            ['JDJ5JDEwJE52dTdpcFAuejh0aUlsNzVPZFV2dC44NnZ1RzZpS01vSElPYzdPN21ib0ZJODU=', '$2y$10$Nvu7ipP.z8tiIl75OdUvt.86vuG6iKMoHIOc7O7mboFI85'],
+            ['MDAwMDAwMDAwMDM=', '00000000003'],
+            //does not end with =
+            ['MDAwMDAwMDAtMDAwMC0wMDAwLTAwMDAtMDAwMDAwMDAwMDAz', '00000000-0000-0000-0000-000000000003'],
+            //end with ==
+            ['Rmlyc3QgTmFtZQ==', 'First Name'],
+        ];
+    }
+}
