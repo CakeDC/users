ad6c651319421a4903707ab94212fcaf4e2b3f76
Alberto Rodriguez <arodu.dev@gmail.com>
2024-02-06 08:47:01 -0400

    cs fix

--- src/Controller/Traits/ProfileTrait.php
+++ src/Controller/Traits/ProfileTrait.php
@@ -13,7 +13,6 @@ declare(strict_types=1);
 
 namespace CakeDC\Users\Controller\Traits;
 
-use Cake\Controller\Component\AuthComponent;
 use Cake\Core\Configure;
 use Cake\Datasource\Exception\InvalidPrimaryKeyException;
 use Cake\Datasource\Exception\RecordNotFoundException;
