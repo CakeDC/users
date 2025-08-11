328b737309b265363677e9285177ba1646abf84e
Vinicius de Almeida Arantes <varantes@MMB031320CPS.local>
2025-08-11 15:09:31 -0300

    Update EmailDelivery
    Allow mailer to be configurable matching all other flows like user
    validation and password resert in the OneTimeLogin flow

--- src/Model/Behavior/OneTimeDelivery/EmailDelivery.php
+++ src/Model/Behavior/OneTimeDelivery/EmailDelivery.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace CakeDC\Users\Model\Behavior\OneTimeDelivery;
 
+use Cake\Core\Configure;
 use Cake\Datasource\EntityInterface;
 use Cake\Mailer\MailerAwareTrait;
 
@@ -37,6 +38,8 @@ class EmailDelivery implements DeliveryInterface
      */
     public function send(EntityInterface $user, string $token): void
     {
-        $this->getMailer('CakeDC/Users.Users')->send('sendToken', [$user, $token]);
+        $this
+            ->getMailer(Configure::read('Users.Email.mailerClass') ?: 'CakeDC/Users.Users')
+            ->send('sendToken', [$user, $token]);
     }
 }
