96da7cd6b9afc178e74381f90fd16a7c30e0013f
Jorge González <steinkel@gmail.com>
2025-10-19 20:31:32 +0100

    fix deprecation

--- src/Model/Behavior/RegisterBehavior.php
+++ src/Model/Behavior/RegisterBehavior.php
@@ -141,15 +141,17 @@ class RegisterBehavior extends BaseTokenBehavior
      * @param \Cake\Event\Event $event event
      * @param \Cake\Validation\Validator $validator validator
      * @param string $name name
-     * @return \Cake\Validation\Validator
+     * @return void
      */
     public function buildValidator(\Cake\Event\EventInterface $event, Validator $validator, $name)
     {
         if ($name === 'default') {
-            return $this->_emailValidator($validator, $this->validateEmail);
+            $event->setResult($this->_emailValidator($validator, $this->validateEmail));
+
+            return;
         }
 
-        return $validator;
+        $event->setResult($validator);
     }
 
     /**
@@ -164,7 +166,7 @@ class RegisterBehavior extends BaseTokenBehavior
         $this->validateEmail = (bool)$validateEmail;
         $validator
             ->add('email', 'valid_email', ['rule' => 'email'])
-            ->notBlank('email', __d('cake_d_c/users', 'This field is required'), function ($context) {
+            ->notBlank('email', __d('cake_d_c/users', 'This field is required'), function () {
                 return $this->validateEmail;
             });
 
