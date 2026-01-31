0f9d7d98240bcf643ddc092db581a64cfbd004a2
Alberto Rodriguez <arodu.dev@gmail.com>
2026-01-31 11:01:17 -0400

    feat: add documentation and test for skipping 2FA verification

--- tests/TestCase/Controller/Traits/OneTimePasswordVerifyTraitTest.php
+++ tests/TestCase/Controller/Traits/OneTimePasswordVerifyTraitTest.php
@@ -16,7 +16,11 @@ namespace CakeDC\Users\Test\TestCase\Controller\Traits;
 use Cake\Core\Configure;
 use Cake\Http\ServerRequest;
 use Cake\ORM\TableRegistry;
+use Cake\Event\Event;
 use CakeDC\Auth\Controller\Component\OneTimePasswordAuthenticatorComponent;
+use CakeDC\Auth\Authentication\AuthenticationService;
+use CakeDC\Auth\Authenticator\TwoFactorAuthenticator;
+use CakeDC\Users\UsersPlugin;
 
 class OneTimePasswordVerifyTraitTest extends BaseTrait
 {
@@ -275,6 +279,51 @@ class OneTimePasswordVerifyTraitTest extends BaseTrait
                 ],
             ],
             $session->read(),
+        );
+    }
+
+    /**
+     * testVerifySkipEventCheck
+     */
+    public function testVerifySkipEventCheck()
+    {
+        Configure::write('OneTimePasswordAuthenticator.login', true);
+        $request = $this->getMockBuilder('Cake\Http\ServerRequest')
+            ->onlyMethods(['is', 'getData', 'getSession'])
+            ->addMethods(['allow'])
+            ->getMock();
+        $this->Trait->setRequest($request);
+
+        $userData = [
+            'id' => 1,
+            'secret_verified' => 1,
+            'email' => 'test@example.com'
+        ];
+        $session = $this->_mockSession([
+            'temporarySession' => $userData,
+        ]);
+
+        $eventMock = $this->getMockBuilder(Event::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $eventMock->method('getResult')->willReturn(true);
+
+        $this->Trait->expects($this->once())
+            ->method('dispatchEvent')
+            ->with(UsersPlugin::EVENT_2FA_SKIP_VERIFY, ['user' => $userData])
+            ->willReturn($eventMock);
+
+        $this->Trait->expects($this->once())
+            ->method('redirect');
+        $this->Trait->verify();
+        
+        $this->assertNull(
+            $session->read(AuthenticationService::TWO_FACTOR_VERIFY_SESSION_KEY)
+        );
+
+        $this->assertEquals(
+            $userData,
+            $session->read(TwoFactorAuthenticator::USER_SESSION_KEY)
         );
     }
 }
